27c1f7c272058b05b93e487270ea578028a593f9
sg <sg@sheltons-mini.lan>
2018-11-03 17:11:28 -0700

    use regex to get controller and action values

--- Core/Router.php
+++ Core/Router.php
@@ -16,13 +16,25 @@ class Router
 
   public function match($url)
   {
-    foreach ($this->routes as $route => $params) {
-      if($url == $route){
-        $this->params = $params;
-        return true;
+    // foreach ($this->routes as $route => $params) {
+    //   if($url == $route){
+    //     $this->params = $params;
+    //     return true;
+    //   }
+    // }
+    
+    $regex = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+    if(preg_match($regex, $url, $matches)){
+      $params = [];
+      foreach($matches as $key => $match){
+        if(is_string($key)){
+          $params[$key] = $match;
+        }
       }
-    }
 
+      $this->params = $params;
+      return true;
+    }
     return false;
   }
 
