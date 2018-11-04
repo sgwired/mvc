d3643dba7ef37d7cc74d988f6e35b8965eb0232e
Shelton Gayles <sgbythelake@gmail.com>
2018-11-04 13:17:13 -0800

    switch to composer autoloader for my classes

--- public/index.php
+++ public/index.php
@@ -12,19 +12,6 @@ require '../vendor/autoload.php';
 Twig_Autoloader::register();
 
 
-/**
- *  Autoloader
- */
-spl_autoload_register(function ($class) {
-  $root = dirname(__DIR__);
-  $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-  if (is_readable($file)){
-    require $root . '/' . str_replace('\\', '/', $class) . '.php';
-  }
-});
-
-
-
 $router = new Core\Router();
 
 // Routing table
