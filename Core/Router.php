051fccbad9b1643998a8ab14632a144d6e565d4f
Shelton Gayles <sgbythelake@gmail.com>
2018-11-04 16:22:23 -0800

    Added error exception handling and custom error pages

--- Core/Router.php
+++ Core/Router.php
@@ -88,7 +88,7 @@ class Router
       }
     } else {
       // echo "No route matched.";
-      throw new \Exception('No route matched.');
+      throw new \Exception('No route matched.', 404);
     }
   }
 
