df7a7a47677ddd2590e3e08fbc385299deaa8d12
Administrator <sheltong@jakks.net>
2022-10-27 13:31:30 -0700

    remove password confirmation replace with hide/show jqurey

--- App/Models/User.php
+++ App/Models/User.php
@@ -83,9 +83,6 @@ class User extends  \Core\Model
             $this->errors[] = "Email is already used";
         }
         
-        if ($this->password != $this->password_confirmation) {
-            $this->errors[] = 'Password must match confirmation';
-        }
         
         if (strlen($this->password) < 6) {
             $this->errors[] = "Please enter at least 6 characters for the password";
